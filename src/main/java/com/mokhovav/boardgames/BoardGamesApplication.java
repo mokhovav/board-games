c1f7425a25aa80f862f361e0e5e51a30b74c95af
mokhovav <andrey.v.mohov@gmail.com>
2020-07-11 08:57:55 +0300

    Added submodule

---
+++ src/main/java/com/mokhovav/boardgames/BoardGamesApplication.java
@@ -0,0 +1,13 @@
+package com.mokhovav.boardgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardGamesApplication.class, args);
+    }
+
+}
